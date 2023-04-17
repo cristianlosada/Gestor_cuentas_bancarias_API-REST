ffdf727400f3fb9e162040614d7e770ae5373bef
Crissoft <cristianlosano314@gmail.com>
2023-04-17 00:59:19 -0500

    ajusta espaciados en archivo principal

--- src/app.py
+++ src/app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
 from flask_cors import CORS, cross_origin
-
 from config import config
-
 # Routes
 from routes import cuentas
 
 app = Flask(__name__)
-
 CORS(app, resources={r"/api/*": {"origins": "*"}})
-
 
 def page_not_found(error):
     return "<h1>Not found page</h1>", 404
@@ -20,7 +16,6 @@ def after_request(response):
     response.headers.remove('Access-Control-Allow-Methods')
     response.headers.remove('Access-Control-Allow-Headers')
     return response
-
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
